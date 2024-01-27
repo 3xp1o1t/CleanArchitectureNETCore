2085b05b7960904fd89bd97de907ba092d6ec4dd
3xp1o1t <jes.monti.503@gmail.com>
2024-01-27 13:19:22 -0600

    Add encapsulation example using User domain

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Users/User.cs
@@ -0,0 +1,25 @@
+using CleanArchitecture.Domain.Abstractions;
+
+namespace CleanArchitecture.Domain.Users;
+
+public sealed class User : Entity
+{
+
+  private User(Guid id, Nombre nombre, Apellido apellido, Email email) : base(id)
+  {
+    Nombre = nombre;
+    Apellido = apellido;
+    Email = email;
+  }
+
+  public Nombre? Nombre { get; private set; }
+  public Apellido? Apellido { get; private set; }
+  public Email? Email { get; private set; }
+
+  public static User Create(Nombre nombre, Apellido apellido, Email email)
+  {
+    var user = new User(Guid.NewGuid(), nombre, apellido, email);
+    return user;
+  }
+
+}
